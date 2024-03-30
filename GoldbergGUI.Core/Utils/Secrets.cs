125791c75bded477d836e91b05d04fe3e9ad5765
Pandoriaantje <pandoriaantje@gmail.com>
2024-03-30 19:21:30 +0100

    Build fix
    fixed all build errors

---
+++ GoldbergGUI.Core/Utils/Secrets.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoldbergGUI.Core.Utils
+{
+    public class Secrets : ISecrets
+    {
+        public string SteamWebApiKey()
+        {
+            return "<ENTER STEAM WEB API KEY HERE>";
+        }
+    }
+}
