3dbebc81fd562b14982fa2dacce9bdc0dafc29f9
Pandoriaantje <pandoriaantje@gmail.com>
2024-03-31 08:56:18 +0200

    added error messagebox of no key found
    added a messagebox if no key.ini is found alongside the executable, with clear instructions.

--- GoldbergGUI.Core/Utils/Secrets.cs
+++ GoldbergGUI.Core/Utils/Secrets.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Windows.Forms;
 
 namespace GoldbergGUI.Core.Utils
 {
@@ -8,11 +9,13 @@ namespace GoldbergGUI.Core.Utils
         public string SteamWebApiKey()
         {
             string executableDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string keyFilePath = Path.Combine(executableDirectory, "key.ini");
+            string keyFilePath = Path.Combine(executableDirectory, "key.txt");
 
             // Check if the key file exists
             if (!File.Exists(keyFilePath))
             {
+                // Show message box informing the user
+                MessageBox.Show("The key.ini file was not found. Make sure to place the key.ini containing your API key, alongside the executable. Generate your personal API key at https://steamcommunity.com/dev/apikey.", "API keyfile Not Found", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 throw new FileNotFoundException("Key file not found.", keyFilePath);
             }
 
